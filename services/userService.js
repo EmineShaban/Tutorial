623795a260d6ce41d0e4233b371f2175bdc1b68d
Emine Shaban <emineshaban@icloud.com>
2022-10-26 22:51:25 +0300

    fixed user services

--- services/userService.js
+++ services/userService.js
@@ -2,5 +2,5 @@ const User = require('../models/User')
 // const Trip = require('../models/Trip')
 
 exports.getOne = (userId) => User.findById(userId)
-exports.addTrip = (userId, tripID) => User.updateOne({_id: userId}, {$push: {offeredHotels: tripID}})
+exports.addcourse = (userId, tripID) => User.updateOne({_id: userId}, {$push: {usersEnrolled: tripID}})
 exports.addHotel = (userId, hotelID) => User.updateOne({_id: userId}, {$push: {bookedHotels: hotelID}})
