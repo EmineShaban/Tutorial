1dae386b5e1c0206ad9e00a12d841e5e6ac5ba51
Emine Shaban <emineshaban@icloud.com>
2022-10-26 22:49:02 +0300

    fixed db

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
 exports.PORT = 3000
-exports.DB_QUERYSTRING = 'mongodb://localhost:27017/BookingUni'
+exports.DB_QUERYSTRING = 'mongodb://localhost:27017/Tutorial'
 exports.SALT_ROUNDS = 10
 exports.SECRET = 'somesecret'
